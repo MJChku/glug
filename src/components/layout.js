b072acfca9abd716711fc51439468d7525d6f995
MJCooper <35918423+MJChku@users.noreply.github.com>
2020-02-04 20:16:37 -0600

    Update layout.js

--- src/components/layout.js
+++ src/components/layout.js
@@ -10,18 +10,7 @@ export default function Layout({ children }) {
   return (
     <ThemeContext.Consumer>
       {theme => (
-        <div>
-            <Navbar />
-            <Box
-              marginTop="90px"
-              minHeight="100%"
-              marginBottom="60px"
-              className="main-container"
-            >
-              {children}
-            </Box>
-            <Footer className="footer"></Footer>
-        </div>
+        
       )}
     </ThemeContext.Consumer>
   )
