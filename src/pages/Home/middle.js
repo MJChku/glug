232ed551fb9e65d142c5423c71d79e19d80fbcc8
MJCooper <35918423+MJChku@users.noreply.github.com>
2020-02-04 19:52:36 -0600

    Update middle.js

--- src/pages/Home/middle.js
+++ src/pages/Home/middle.js
@@ -44,18 +44,14 @@ const Pic = styled(Image)({})
 const Middle = () => (
   <Container>
     <Box width={[1, 1 / 2]} alignSelf="center" p={3}>
-      <Heading>Purpose</Heading>
-      <Right>
-        The purpose of a GLUG is to equip students with FOSS and make them
-        understand how FOSS is developed and why is it done so. While doing that
-        - how one can contribute back to FOSS.It is a gathering of like minded
-        people, where people speak about Freedom Software Philosophy and create
-        awareness about the ‘usage’ of Freedom Software.
-      </Right>
+      <Heading>About...</Heading>
+      <Center>
+        The site is dedicated for my post.
+      </Center>
     </Box>
     <Box width={[1, 1 / 2]} alignSelf="center" p={3}>
       <Left>
-        <Pic filename="open_source.png" />
+        <Pic filename="teaser.png" />
       </Left>
     </Box>
   </Container>
