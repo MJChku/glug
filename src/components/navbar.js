a7d43a99512c0bd99be2f6eda4a199f13530de94
MJCooper <35918423+MJChku@users.noreply.github.com>
2020-02-04 20:03:10 -0600

    Update navbar.js

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -55,7 +55,7 @@ const Header = ({ siteTitle }) => (
         <Box mx="auto" />
         <MenuItems p={[6, 2, 2]}>
           <Links variant="nav" to="/blog" activeClassName="borderMarker">
-            <Text fontSize={[3, 4, 4]}>Littel Story</Text>
+            <Text fontSize={[3, 4, 4]}>Little Story</Text>
           </Links>
         </MenuItems>
       </NavBar>
